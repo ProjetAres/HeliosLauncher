ac8c167eb10b6995d79fee94462b8c0e18b88f55
Vulcain <vulcain-7347@protonmail.com>
2020-08-13 02:05:03 +0200

    Changed mojang URL to dev URL

--- app/assets/js/mojang.js
+++ app/assets/js/mojang.js
@@ -14,43 +14,15 @@ const minecraftAgent = {
     name: 'Minecraft',
     version: 1
 }
-const authpath = 'https://authserver.mojang.com'
+// const authpath = 'https://authserver.mojang.com'
+const authpath = 'http://ares.lul.tf/auth'
+
 const statuses = [
     {
-        service: 'sessionserver.mojang.com',
+        service: 'ares.lul.tf',
         status: 'grey',
         name: 'Multiplayer Session Service',
         essential: true
-    },
-    {
-        service: 'authserver.mojang.com',
-        status: 'grey',
-        name: 'Authentication Service',
-        essential: true
-    },
-    {
-        service: 'textures.minecraft.net',
-        status: 'grey',
-        name: 'Minecraft Skins',
-        essential: false
-    },
-    {
-        service: 'api.mojang.com',
-        status: 'grey',
-        name: 'Public API',
-        essential: false
-    },
-    {
-        service: 'minecraft.net',
-        status: 'grey',
-        name: 'Minecraft.net',
-        essential: false
-    },
-    {
-        service: 'account.mojang.com',
-        status: 'grey',
-        name: 'Mojang Accounts Website',
-        essential: false
     }
 ]
 
